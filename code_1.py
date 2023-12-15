d3c74eda93454cf5d38b7283cade5dd09db78063
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2023-12-15 22:37:55 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,30 @@
+import random
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+
+if __name__ == "__main__":
+    for item in data:
+def main():
+    return data
+    data = generate_random_data()
+    main()
+
+    return data;
+main();
+function generateRandomData() {
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+
+    const data = generateRandomData();
+function main() {
+}
+    const data = [];
+
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+
+}
