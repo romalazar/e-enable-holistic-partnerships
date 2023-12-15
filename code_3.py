b48cf104dd6b38a617a970c6ced2007cbba3a181
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2023-12-15 19:35:39 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,58 @@
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+import random
+def main():
+    for item in data:
+
+    return data
+if __name__ == "__main__":
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+main();
+    return data;
+        console.log("Random Number: " + item);
+}
+function main() {
+
+function generateRandomData() {
+    const data = [];
+}
+    const data = generateRandomData();
+
+    }
+
+    });    const data = [];
+}
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+
+
+    data.forEach(item => {
+    });
+    return data;
+main();
+    }
+}
+
+function main() {
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+            System.out.println("Random Number: " + item);
+
+        for (int item : data) {
+        }
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+        }
+        for (int i = 0; i < 10; i++) {
+    }
+}
+public class RandomDataGenerator {
